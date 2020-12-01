64e0a7cf01d3f562a9041e835f35981111bb9c10
sokorahen-szk <kamonotakahashi@gmail.com>
2020-12-01 17:33:05 +0900

    js修正

--- js/base.js
+++ js/base.js
@@ -1,7 +1,31 @@
 $( () => {
 
+  // ハンバーガー
   $('.hamburger-menu').on('click', function() {
     $(this).toggleClass('hamburger-menu-active')
-  })
+  });
 
+  $(window).resize(function() {
+    addCardDummy();
+  });
+
+
+  addCardDummy();
 });
+
+function addCardDummy() {
+  const length = $('.card').length;
+  const count = ($(window).width() >= 1400) ? length % 4 : length % 3;
+  const add = Math.abs( (($(window).width() >= 1400) ? 4 : 3) - count);
+
+  if (add !== 0) {
+    if ($('.card.hide').length < add) {
+      for (let i = 0; i < add; i++) {
+        $('.container__wrap').append(`<div class="card hide"></div>`);
+      }
+    }
+  } else {
+    $('.card.hide').remove();
+  }
+
+}
